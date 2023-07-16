c58125311affe21518086ae7c0c702bd4bc02cd7
Yusuf <kurakiorg@gmail.com>
2023-07-16 13:50:30 +0300

    Str fix

--- manga/models.py
+++ manga/models.py
@@ -12,22 +12,37 @@ class FixModel(models.Model):
 class Language(FixModel):
     name = models.CharField(max_length=25)
 
+    def __str__(self):
+        return self.name
+
 class Publisher(FixModel):
     name = models.CharField(max_length=75)
     description = models.TextField()
     website = models.CharField(max_length=35)
     logo = models.ImageField(upload_to='manga/publisher/')
 
+    def __str__(self):
+        return self.name
+
 class Artist(FixModel):
     name = models.CharField(max_length=75)
     description = models.TextField()
+
+    def __str__(self):
+        return self.name
 
 class Author(FixModel):
     name = models.CharField(max_length=75)
     description = models.TextField()
 
+    def __str__(self):
+        return self.name
+
 class Category(FixModel):
     name = models.CharField(max_length=75)
+
+    def __str__(self):
+        return self.name
 
 class Manga(FixModel):
     name = models.CharField(max_length=255)
@@ -41,9 +56,15 @@ class Manga(FixModel):
     author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True)
     purchase_link = models.URLField()
 
+    def __str__(self):
+        return f'{self.name} - {self.original_name}'
+
 class MangaVolume(FixModel):
     manga = models.ForeignKey(Manga, on_delete=models.SET_NULL, null=True)
     language = models.ForeignKey(Language, on_delete=models.SET_NULL, null=True)
     vol_num = models.PositiveSmallIntegerField()
     vol_name = models.CharField(max_length=35)
     cover = models.ImageField(upload_to='manga/covers/')
+
+    def __str__(self):
+        return f'[{self.language}] {self.manga} - {self.vol_num} - {self.vol_name}'
