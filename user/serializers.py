dc60757f2c19c86a5e78a6d611883263d65129fa
Yusuf <kurakiorg@gmail.com>
2023-11-04 21:05:53 +0300

    Register "TypeError at /user/register/" error fix

--- user/serializers.py
+++ user/serializers.py
@@ -6,8 +6,8 @@ from django.contrib.auth.password_validation import validate_password
 class RegistrationSerializer(serializers.ModelSerializer):
     username = serializers.CharField(required=True, validators=[UniqueValidator(queryset=User.objects.all())])
     email = serializers.EmailField(required=True, validators=[UniqueValidator(queryset=User.objects.all())], style={'input_type': 'email'})
-    password = serializers.CharField(required=True,write_only=True,validators=[validate_password], style={'input_type': 'password'})
-    password2 = serializers.CharField(required=True,write_only=True,validators=[validate_password], style={'input_type': 'password'})
+    password = serializers.CharField(required=True, write_only=True, validators=[validate_password], style={'input_type': 'password'})
+    password2 = serializers.CharField(write_only=True, style={'input_type': 'password'})
 
     class Meta:
         model = User
@@ -16,13 +16,13 @@ class RegistrationSerializer(serializers.ModelSerializer):
     def validate(self, attrs):
         if attrs['password'] == attrs['password2']:
             return super().validate(attrs)
-        raise serializers.ValidationError({'password' : 'Please check your passwords.'})
+        raise serializers.ValidationError({'password': 'Please check your passwords.'})
 
     def create(self, validated_data):
         user = User.objects.create(
-            username = validated_data.get('username'),
-            email = validated_data.get('email'),
+            username=validated_data.get('username'),
+            email=validated_data.get('email'),
         )
         user.set_password(validated_data.get('password'))
         user.save()
-        return super().create(validated_data)
+        return user
